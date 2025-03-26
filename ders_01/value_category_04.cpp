13a3524e41b46b64515ad208d2592915cc74770b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-03-26 12:40:39 +0300

    Update and rename value_category_04.txt to value_category_04.cpp

--- ders_01/value_category_04.cpp
+++ ders_01/value_category_04.cpp
@@ -1,13 +1,16 @@
-struct Nrc {};
+struct Nec {};
 
 Nec nec;
 const Nec cnec;
 
+/*
                         PARAMS FOR OVERLOADS
 
 argument                Nec&      const Nec&       Nec&&      const Nec&&
 ===========             =====     ==========       =====      ===========
 nec                        1           2             X             X               
-cnec
-Nec{}
-std::move(cnec)
+cnec                       X           1             X             X
+Nec{}                      X           3             1             2
+std::move(cnec)            X           2             X             1     
+
+*/
